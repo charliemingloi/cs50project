296eb2384bfdea81c7704ca1e20a6b68f4edcd9b
charliemingloi <51398428+charliemingloi@users.noreply.github.com>
2023-11-13 19:15:29 +0800

    Files

---
+++ app.py
@@ -0,0 +1,174 @@
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+from cs50 import SQL
+from helpers import login_required
+import datetime
+import cv2
+import pytesseract
+from mytestfiles import image_to_text
+
+app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+db = SQL("sqlite:///history.db")
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/")
+@login_required
+def index():    
+    
+    name = image_to_text()
+    users = db.execute("SELECT * FROM user_info WHERE car_plate_no=(?)", name)
+    for i in range(len(users)):
+        db.execute("INSERT INTO info (id, real_name, CLASS, car_plate_no, date) VALUES (?,?,?,?,?)", users[i]["id"], users[i]["real_name"], users[i]["class"], name, datetime.datetime.now())
+
+    return render_template("index.html", placeholder=image_to_text())
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if "user_id" in session:
+        return render_template("errorfile.html")
+    if request.method == "POST":
+        name = request.form.get("name")
+        password = request.form.get("password")
+        if not name or not password:
+            flash("Invalid Username/ Invalid Password")
+            return redirect("/login")
+        rows = db.execute("SELECT * FROM users WHERE username=(?)", name)
+        if (len(rows) != 1 or not check_password_hash(rows[0]["password"], password)):
+            flash("Invalid Password")
+            return redirect("/login")
+            
+        session["user_id"] = rows[0]["id"]
+
+        return redirect("/")
+    else:
+        return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if "user_id" in session:
+        return render_template("errorfile.html")
+    if request.method == "POST":
+        name = request.form.get("name")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+        if (password != confirmation):
+            flash("Invalid password")
+            return redirect("/register")
+
+        else:
+            size = db.execute("SELECT username FROM users WHERE username=(?)", name)
+            if (len(size) == 1):
+                flash('Username is Used')
+                return redirect("/register")
+            else:
+                hash_password = generate_password_hash(password)
+                values = db.execute("INSERT INTO users(username, password, registered_date) VALUES (?,?,?)", name, hash_password, datetime.datetime.now())
+                flash('Registered!')
+                session["user_id"] = values
+                return redirect("/")
+
+    else:
+        return render_template("register.html")
+
+@app.route("/forgotpassword", methods=["GET", "POST"])
+def forgotpassword():
+    if "user_id" in session:
+        return render_template("errorfile.html")
+    if request.method == "POST":
+        name = request.form.get("name")
+        password = request.form.get("password")
+        length = db.execute("SELECT username FROM users WHERE username=(?)", name)
+        if len(length) != 1:
+            flash("Non registered User")
+            return redirect("/forgotpassword")
+        else:
+            hashed = generate_password_hash(password)
+            db.execute("UPDATE users SET password=(?)", hashed)
+            return redirect("/login")
+    else:
+        return render_template("forgotpassword.html")
+
+@app.route("/profile", methods=["GET", "POST"])
+def profile():
+    if "user_id" not in session:
+        return render_template("errorfile.html")
+    ids = session["user_id"]
+    name = db.execute("SELECT username FROM users WHERE id=(?)", ids)
+    registered_date = db.execute("SELECT registered_date FROM users WHERE id=(?)", ids)
+
+    if request.method == "POST":
+        real_name = request.form.get("real_name")
+        # Cannot use class since its a method in Python
+        classes = request.form.get("classes")
+        student_id = request.form.get("student_id")
+        car_plate = request.form.get("car_plate_no")
+        
+        check_length = db.execute("SELECT id FROM user_info WHERE id=(?)", ids)
+        if len(check_length) == 1:
+            try:
+                db.execute("UPDATE TABLE user_info SET real_name=(?), student_id=(?), class=(?), car_plate_no=(?)", real_name, classes, student_id, car_plate)
+            except:
+                return render_template("profile.html", name=name[0]["username"], real_name= request.form.get("real_name", "Student Name"), registered_date=registered_date[0]["registered_date"], classes = request.form.get("classes", "Class"), student_id = request.form.get("student_id", "Student No") ,car_plate_no = request.form.get("car_plate_no", "Car Plate"))
+        elif len(check_length) == 0:
+            db.execute("INSERT INTO user_info VALUES(?, ?, ?, ?, ?, ?)", ids, real_name, student_id, classes, car_plate, registered_date[0]["registered_date"])
+        
+        return render_template("profile.html", name=name[0]["username"], real_name= request.form.get("real_name", "Student Name"), registered_date=registered_date[0]["registered_date"], classes = request.form.get("classes", "Class"), student_id = request.form.get("student_id", "Student No") ,car_plate_no = request.form.get("car_plate_no", "Car Plate"))
+
+        
+    else:
+        check_length = db.execute("SELECT id FROM user_info WHERE id=(?)", ids)
+        if len(check_length) == 1:
+            informations = db.execute("SELECT real_name, student_id, class, car_plate_no FROM user_info WHERE id=(?)", ids)
+            return render_template("profile.html", name=name[0]["username"], real_name=informations[0]["real_name"], student_id=informations[0]["student_id"], classes=informations[0]["class"], car_plate_no=informations[0]["car_plate_no"], registered_date=registered_date[0]["registered_date"])
+        else:
+            return render_template("profile.html", name=name[0]["username"], real_name= request.form.get("real_name", "Student Name"), registered_date=registered_date[0]["registered_date"], classes = request.form.get("classes", "Class"), student_id = request.form.get("student_id", "Student No") ,car_plate_no = request.form.get("car_plate_no", "Car Plate"))
+
+@app.route("/deregister", methods=["POST", "GET"])
+def deregister():
+    if request.method == "POST":
+        name = request.form.get("name")
+        password = request.form.get("password")
+        check_name = db.execute("SELECT username FROM users WHERE username=(?)", name)
+        if (len(check_name) != 1):
+            flash("Invalid User")
+            return redirect("/deregister")
+        else:
+            check_password = db.execute("SELECT password FROM users WHERE username=(?)", name)
+            if (check_password_hash(check_password[0]["password"], password)):
+                id_selected = db.execute("SELECT id FROM users WHERE username=(?)", name)
+                session.clear()
+                db.execute("DELETE FROM users WHERE username=(?)", name)
+                db.execute("DELETE FROM user_info WHERE id=(?)", id_selected[0]["id"])
+                return redirect("/")
+
+            else:
+                flash("Invalid Password")
+                return redirect("/deregister")
+    else:
+        return render_template("deregister.html")
+
+
+@login_required
+@app.route("/logs")
+def logs():    
+    logs = db.execute("SELECT * FROM info")
+    return render_template("logs.html", logs=logs)
--- mytestfiles.py
+++ mytestfiles.py
@@ -1,24 +1,39 @@
 import cv2
 import pytesseract
 
-# Load the image using OpenCV
-image_path = 'random.jpeg'
-image = cv2.imread(image_path)
+def image_to_text():
+    # Load the image using OpenCV
+    image_path = 'random.jpeg'
+    image = cv2.imread(image_path)
 
-cv2.imshow('Image',image)
+    # Convert the image to grayscale for better OCR results
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# Convert the image to grayscale for better OCR results
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cv2.imshow('Gray',gray_image)
-# Apply image preprocessing (e.g., thresholding, blurring) to enhance text
-processed_image = cv2.GaussianBlur(gray_image, (7, 7), 3)
-cv2.imshow('Gaussion', processed_image)
-processed_image = cv2.threshold(processed_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-text = pytesseract.image_to_string(processed_image,config='--psm 6')
-print(text)
-cv2.imshow('Images', processed_image)
-cv2.imwrite('newimg.png', processed_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    # Apply image preprocessing to enhance text
+    processed_image = cv2.GaussianBlur(gray_image, (7, 7), 3)
+    processed_image = cv2.threshold(processed_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
-#https://stackoverflow.com/questions/44619077/pytesseract-ocr-multiple-config-options ,
+    # Use Tesseract OCR to extract text
+    text = pytesseract.image_to_string(processed_image, config='--psm 7')
+
+    # Perform additional post-processing if needed
+    newtext = ""
+    count = 0
+    for i in range(len(text)):
+        if text[i].isalpha():
+            newtext += text[i]
+        elif count == 0 and text[i].isdigit():
+            count += 1
+            newtext += " "
+            newtext += text[i]
+        elif text[i].isdigit():
+            count += 1
+            newtext += text[i]
+
+    return newtext
+
+# Specify the path to your image
+
+
+# Call the function and print the result
+
